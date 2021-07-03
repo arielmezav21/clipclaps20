02b8240c1e289c2b51b3a0f5efda180ba7bf0b0c
arielmezav21 <86581379+arielmezav21@users.noreply.github.com>
2021-07-03 01:39:37 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 
 //xxx Wajib Diisi
 
-$token = "ZDgyZjYwZGQtYzc1NC00OTM3LWJhYzktMDE4ZTAxNjk3ZTAw";
-$uuid = "287df28c-4042-4d3c-a17b-253b3cce3107";
-$userid = "27098744";
+$token = "MGRjODc4NTMtYTEyNC00OGRlLThkMWQtMzllNjU1NWI3ZjZh";
+$uuid = "a9384330d50c4544";
+$userid = "4977487";
 
-$eksternal_version = "2.0.1";
+$eksternal_version = "2.9.3.2";
 $version = "42";
 $device = "8.1.0";
-$timezone = "7";
-$lang = "id";
+$timezone = "-4";
+$lang = "es";
 
 $spin_version = "40";
-$user_agent = "Mozilla/5.0 (Linux; Android 8.1.0; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
+$user_agent = "okhttp/4.2.1 (Linux; Android 8.1.0; Redmi 6 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.127 Mobile Safari/537.36"; //Search in Google Chrome With Keyword "my user agent"
